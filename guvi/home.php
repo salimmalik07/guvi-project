32728ea839a517ad134eac89a252dd88eefb2e44
SALIM MALIK S <42864470+salimmalik07@users.noreply.github.com>
2020-11-19 15:27:37 +0530

    Update home.php

--- guvi/home.php
+++ guvi/home.php
@@ -7,10 +7,21 @@ if(!isset ($_SESSION ['username'])) {
 <html>
     <head>
         <title> home page</title>
+        <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
 </head>
 <body>
     <a href="logout.php" style="float:right">LOG OUT</a>
+    <div class="container">
+  <div class="row">
+    <div class="col"><?php  echo $_SESSION ['name']; ?></div>
+    <div class="col"><?php  echo $_SESSION ['age']; ?></div>
+    <div class="col"><?php  echo $_SESSION ['username']; ?></div>
+  </div>
+</div>
 
-    <h1> welcome <?php  echo $_SESSION ['username']; ?> </h1>
 </body>
 </html>
